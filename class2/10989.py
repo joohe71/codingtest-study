cfd629585e6030b2ee4bc489da0ebe917483b1d0
joohe71 <joohe71@gmail.com>
2022-02-05 02:31:35 +0900

    update

---
+++ class2/10989.py
@@ -0,0 +1,12 @@
+# # 메모리 초과
+# import sys
+
+# n = int(input())
+# li = []
+
+# for _ in range(n):
+#     num = int(sys.stdin.readline())
+#     li.append(num)
+
+# for i in sorted(li):
+#     print(i)
