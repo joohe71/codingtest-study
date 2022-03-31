c93cf9f5f70104600b39fd7ce01208a03a25f551
joohe71 <joohe71@gmail.com>
2022-03-31 23:37:24 +0900

    Update level2

--- programmers/level2/124 나라의 숫자.py
+++ programmers/level2/124 나라의 숫자.py
@@ -10,3 +10,15 @@ def solution(n):
             answer += str(r)
             n = p
     return answer[::-1]
+
+# =========================
+# 다른 풀이
+def solution(n):
+    answer = ''
+    nums = [1,2,4]
+    
+    while n > 0:
+        n -= 1
+        answer += str(nums[n%3])
+        n = n // 3
+    return answer[::-1]
