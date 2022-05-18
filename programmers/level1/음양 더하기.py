45290cfbcf2fcc6bbecc5d69d0eb58b821f6069e
joohe71 <joohe71@gmail.com>
2022-05-19 00:30:46 +0900

    Update

--- programmers/level1/음양 더하기.py
+++ programmers/level1/음양 더하기.py
@@ -1,3 +1,11 @@
+# 더 간결한 코드
+def solution(absolutes, signs):
+    answer = 0
+    for index, num in enumerate(absolutes):
+        answer += num if signs[index] else -num
+    return answer
+
+
 def solution(absolutes, signs):
     for i in range(len(signs)):
         if not signs[i] :
