81904372bd5629b9cf4353220f4957b0b228e45d
joohe71 <joohe71@gmail.com>
2022-05-18 23:15:54 +0900

    Update

---
+++ programmers/level1/두 개 뽑아서 더하기.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            num = numbers[i] + numbers[j]
+            if num in answer:
+                pass
+            else:
+                answer.append(num)
+            
+    answer.sort()
+    return answer
